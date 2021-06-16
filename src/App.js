624adb12d33e5754e9bee5ffd4d9770b84606b8a
RikvanderSchalie <79224832+RikvanderSchalie@users.noreply.github.com>
2021-06-16 16:07:23 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { useState, createContext  } from "react";
+import "./App.css";
+import { Switch, Route, Link } from "react-router-dom";
+import Counters from "./pages/Counters";
+import Checkout from "./pages/Checkout";
+import Home from "./pages/Home";
+
+const fruitCounterContext = createContext({});
+
+
+function App() {
+  const [apples, setApples] = useState(0);
+  const [bananas, setBananas] = useState(0);
+  const [pears, setPears] = useState(0);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/counters">Bestellen</Link>
+            </li>
+            <li>
+              <Link to="/checkout">Betalen</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/counters">
+            <Counters
+              apples={apples}
+              setApples={setApples}
+              bananas={bananas}
+              setBananas={setBananas}
+              pears={pears}
+              setPears={setPears}
+            />
+          </Route>
+          <Route path="/checkout">
+            <Checkout pears={pears} apples={apples} bananas={bananas} />
+          </Route>
+          <Route path="/">
+            <Home totalFruit={pears + apples + bananas} />
+          </Route>
+        </Switch>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/todo.md
@@ -0,0 +1,8 @@
+1 [x] Create context importeren
+2 [x] context aanmaken
+3 [] context provider om de app heenzetten
+4 [] Values doorgeven
+5 [] in elk component
+    -[] usecontext importeren
+    -[] context object importeren
+    -[] de waardes uit de context halen (ipv props gebruiken)
